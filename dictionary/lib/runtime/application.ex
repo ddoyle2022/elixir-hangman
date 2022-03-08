a6707f58396820ad716423f82137c6c21c6d59ee
ddoyle2022 <daniel.doyle@fastradius.com>
2022-03-08 12:03:57 -0600

    added supervisor to manage Dictionary agent

--- dictionary/lib/runtime/application.ex
+++ dictionary/lib/runtime/application.ex
@@ -4,6 +4,14 @@ defmodule Dictionary.Runtime.Application do
 
   # Like main() in OOP/procedural languages
   def start(_type, _args) do
-    Dictionary.Runtime.Server.start_link()
+    children = [
+      { Dictionary.Runtime.Server, [] }
+    ]
+    options = [
+      name: Dictionary.Runtime.Supervisor,
+      strategy: :one_for_one,
+    ]
+    Supervisor.start_link(children, options)
+
   end
 end
